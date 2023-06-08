52cd18e14c39398056230b12d7c7d032743cc549
RobertB512 <87499276+RobertB512@users.noreply.github.com>
2023-06-08 15:33:14 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,13 @@
+# My portfolio website
+
+This is my portfolio website to show off my skills, tell people a little about myself and display some projects showing what I can do.
+
+## Attributions
+
+- Plate image by [naobim](https://pixabay.com/users/naobim-1099306/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6390880) on [Pixabay](https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=6390880)
+- Egg and toast image by [AlexMile](https://pixabay.com/users/alexmile-17401260/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5709759) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=5709759)
+- Cup of coffee image by: [Clker-Free-Vector-Images](https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=312524) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=312524)
+- Headphones image by [Christian Dorn](https://pixabay.com/users/conmongt-1226108/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4011737) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4011737)
+- Hand image by: [Clker-Free-Vector-Images](https://pixabay.com/users/clker-free-vector-images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=303680) from [Pixabay](https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=303680)
+- Skill icons and social icons by [Freepik](https://www.flaticon.com/free-icons/javascript) from Flaticons
+- Ambient sound by [Pixabay](https://pixabay.com/?utm_source=link-attribution&utm_medium=referral&utm_campaign=music&utm_content=20201)
---
+++ scripts/scripts.js
@@ -0,0 +1,109 @@
+// animation content area
+let contentContainers = document.querySelectorAll(".select-item");
+let orderedItemBoxes = document.querySelectorAll(".ordered-item-box");
+let tableSectionHeadings = document.querySelectorAll(".table-section-heading");
+let coffeeBox = document.querySelector(".coffee-box");
+let aboutMe = document.querySelector(".about-me");
+let foodBox = document.querySelector(".food-box");
+let food = document.querySelector(".food");
+let likesInterests = document.querySelector(".likes-interests");
+let tablet = document.querySelector(".tablet");
+let backToTableBtn = document.querySelector(".back-to-table");
+
+// tablet content
+let tabletContent = document.querySelector(".tablet-content");
+let form = document.querySelector("form")
+let errorName = document.querySelector(".error-name")
+let errorEmail = document.querySelector(".error-email")
+let enteredName = document.querySelector("#name")
+let enteredEmail = document.querySelector("#email")
+let enteredMsg = document.querySelector("#message")
+let messageSent = document.querySelector(".message-sent")
+
+// audio selection area
+let backgroundAudio = document.querySelector("audio");
+let soundArea = document.querySelector(".sound-box");
+let soundBtn = document.querySelector(".play-and-pause");
+let playing = false;
+
+// some presets
+aboutMe.classList.add("hide-content");
+likesInterests.classList.add("hide-content");
+backToTableBtn.classList.add("hide-content");
+tabletContent.classList.add("hide-content")
+
+// trigger animation area
+let animTableItem = (item, showTextContent, animClass) => {
+	item.addEventListener("click", () => {
+		item.classList.add(animClass);
+		if (item.querySelector(".food")) {
+			food.classList.add("anim-food");
+		}
+
+		for (let tableSectionHeading of tableSectionHeadings) {
+			tableSectionHeading.classList.remove("show-content");
+			tableSectionHeading.classList.add("hide-content");
+		}
+
+		showTextContent.classList.remove("hide-content");
+		showTextContent.classList.add("show-content");
+		backToTableBtn.classList.remove("hide-content");
+		backToTableBtn.classList.add("show-content");
+	});
+};
+
+animTableItem(coffeeBox, aboutMe, "anim-order");
+animTableItem(foodBox, likesInterests, "anim-order");
+animTableItem(tablet, tabletContent, "anim-tablet");
+
+// go back to table
+backToTableBtn.addEventListener("click", () => {
+	for (let orderedItemBox of orderedItemBoxes) {
+		orderedItemBox.classList.remove("anim-order");
+	}
+
+	aboutMe.classList.remove("show-content");
+	aboutMe.classList.add("hide-content");
+	likesInterests.classList.remove("show-content");
+	likesInterests.classList.add("hide-content");
+	tabletContent.classList.remove("show-content");
+	tabletContent.classList.add("hide-content");
+
+	for (let tableSectionHeading of tableSectionHeadings) {
+		tableSectionHeading.classList.remove("hide-content");
+		tableSectionHeading.classList.add("show-content");
+	}
+
+	food.classList.remove("anim-food");
+	tablet.classList.remove("anim-tablet");
+	backToTableBtn.classList.remove("show-content");
+	backToTableBtn.classList.add("hide-content");
+});
+
+
+// form validation
+form.addEventListener("submit", (e) => {
+  if (enteredName.validity.valid && enteredName.validity.valid && enteredMsg.validity.valid) {
+    messageSent.textContent = "thank you"
+    enteredName.value = ""
+    enteredEmail.value = ""
+    enteredMsg.value = ""
+  } else {
+    e.preventDefault()
+  }
+})
+
+
+// for the background sound
+soundBtn.addEventListener("click", () => {
+	playing ? backgroundAudio.pause() : backgroundAudio.play();
+
+	backgroundAudio.onplaying = () => {
+		playing = true;
+		soundBtn.textContent = "PAUSE";
+	};
+	backgroundAudio.onpause = () => {
+		playing = false;
+		soundBtn.textContent = "PLAY";
+	};
+});
